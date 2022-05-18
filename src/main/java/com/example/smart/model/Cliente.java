9f4ce34f92fdb9f8098c06c2d4625c900055bcbd
ramdy roa jimenez <ram@192.168.1.8>
2022-05-18 18:33:27 -0500

    update

--- src/main/java/com/example/smart/model/Cliente.java
+++ src/main/java/com/example/smart/model/Cliente.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 public class Cliente {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  int id;
     private String nombre;
     @Column(name="ap_paterno")
